def9d42f78121a5bf88662e53a60794b06345981
jrpearce <jpearce@minderoo.org>
2022-03-10 02:56:06 +0000

    new QC pipeline

--- scripts/QC_2.sh
+++ scripts/QC_2.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 
-source /data/software/miniconda3/etc/profile.d/conda.sh
+source /home/jpearce/miniconda3/etc/profile.d/conda.sh
 
 #"$(conda shell.bash hook)"
 conda activate amplicon
 
-input_directory=$1
-assay1=$2
-assay2=$3
-output_directory=04-qc_2/test/${assay1}
+input_directory=01-demultiplexed
+assay1=$1
+assay2=$2
+output_directory=02-qc/${assay1}
 reads=$(ls $input_directory/${assay1}/*f*)
+
+# Make directories
+mkdir 02-qc/${assay1}
+mkdir 02-qc/${assay2}
 
 # FastQC to check the quality of the demultiplexed samples;
 fastqc $reads \
        -o $output_directory #\
-#       -t 16
+#
 
 #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::#
 
@@ -23,10 +27,10 @@ wait # fastqc results need to be available before continuing
 
 #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::#
 
-input_directory=$1
-assay1=$2
-assay2=$3
-output_directory=04-qc_2/test/${assay2}
+input_directory=01-demultiplexed
+assay1=$1
+assay2=$2
+output_directory=02-qc/${assay2}
 reads=$(ls $input_directory/${assay2}/*f*)
 
 # FastQC to check the quality of the demultiplexed samples;
@@ -42,13 +46,13 @@ wait # fastqc results need to be available before continuing
 
 # MultiQC to create nice output of first assay data
 
-source /data/software/miniconda3/etc/profile.d/conda.sh
+source /home/jpearce/miniconda3/etc/profile.d/conda.sh
 
 #"$(conda shell.bash hook)"
 conda activate multiqc
 
-input_directory=04-qc_2/test/${assay1}
-output_directory=04-qc_2/test/${assay1}/MultiQC
+input_directory=02-qc/${assay1}
+output_directory=02-qc/${assay1}/MultiQC
 
 # MuliQC to check the quality overall samples;
 multiqc $input_directory \
@@ -62,8 +66,8 @@ wait # fastqc results need to be available before continuing
 
 # MultiQC on second assay data
 
-input_directory=04-qc_2/test/${assay2}
-output_directory=04-qc_2/test/${assay2}/MultiQC
+input_directory=02-qc/${assay2}
+output_directory=02-qc/${assay2}/MultiQC
 
 # MuliQC to check the quality overall samples;
 multiqc $input_directory \
