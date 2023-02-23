e3ec018315314d9b608f0b9a7e3c6a6cb0cc178b
Philipp Bayer <philippbay@gmail.com>
2023-02-23 13:09:39 +0800

    Last verbose log

--- scripts/00-setup.sh
+++ scripts/00-setup.sh
@@ -96,6 +96,7 @@ cp -r ${SCRIPT_DIR}/* scripts
 # Set up the blast database
 #...............................................................................................
 
+echo 'Setting up the BLAST database'
 cp ${SCRIPT_DIR}/../resources/* databases/
 gunzip databases/12S.v0.7.16S.v0.2.fasta.gz
 
