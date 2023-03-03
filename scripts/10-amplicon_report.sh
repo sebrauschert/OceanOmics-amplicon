63e718a05dd4347d2bee516c382da81c0e2edab4
Adam Bennett <adbennett@minderoo.org>
2023-03-03 08:26:59 +0000

    small change

--- scripts/10-amplicon_report.sh
+++ scripts/10-amplicon_report.sh
@@ -10,20 +10,18 @@ usage()
     printf "Usage: $0 -v <voyageID>\t<string>\n\t\t\t -a <assay; use flag multiple times for multiple assays>\t<string>\n\t\t\t -r <sequencing_run; can be left blank> \n\n";
     exit 1;
 }
-while getopts v:a:r:w: flag
+while getopts v:a:r: flag
 do
     case "${flag}" in
         v) voyageID=${OPTARG};;
         a) assay+=("$OPTARG");;
         r) sequencing_run=${OPTARG};;
-        w) wd=${OPTARG};;
         *) usage;;
     esac
 done
 
 if [ "${voyageID}" == ""  ]; then usage; fi
 if [ "${assay[1]}" == ""  ]; then usage; fi
-if [ "${wd}" == "" ]; then wd=$(pwd); fi
 
 # For the containerised version: if the ANALYSIS path is present,
 # change to the ANALYSIS directory
@@ -57,4 +55,4 @@ random_samples="${random_samples:1}"
 
 Rscript -e "rmarkdown::render('scripts/report/amplicon_report.Rmd',params=list(voyage = '${voyageID}', assays = '${assay_rmd_input}', random_samples = '${random_samples}', sequencing_run = '${sequencing_run}'))"
 
-mv ${wd}/scripts/report/amplicon_report.html ${wd}/06-report
+mv scripts/report/amplicon_report.html 06-report
